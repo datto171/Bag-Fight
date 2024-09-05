60f260fbc2babde030a383fe93fdfbcabe7cbc25
dattq <dattq@ftech>
2024-09-05 17:10:15 +0700

    update

--- Assets/Scripts/Tower/Player.cs
+++ Assets/Scripts/Tower/Player.cs
@@ -9,19 +9,28 @@ namespace TowerDefense
     public class Player : MonoBehaviour
     {
         List<Enemy> enemies;
+        
 
-        // Start is called before the first frame update
         void Start()
         {
             enemies = new List<Enemy>();
         }
+
+        private void FixedUpdate()
+        {
+            if (enemies.Count > 0)
+            {
+                
+            }
+        }
+
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.GetComponent<Enemy>())
             {
                 Debug.Log(enemies.Count);
-                
+
                 var enemy = other.gameObject.GetComponent<Enemy>();
                 enemies.Add(enemy);
             }
